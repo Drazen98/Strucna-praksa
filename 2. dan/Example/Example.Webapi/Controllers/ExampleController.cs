018c4c0594c66d2ad29ca8fd5bde2c365e2133b1
DESKTOP-L6053DB\Zendra <drazensokcevic32@gmail.com>
2020-07-15 09:39:16 +0200

    2.verzija

--- 2. dan/Example/Example.Webapi/Controllers/ExampleController.cs
+++ 2. dan/Example/Example.Webapi/Controllers/ExampleController.cs
@@ -9,37 +9,57 @@ namespace Example.Webapi.Controllers
 {
     public class ExampleController : ApiController
     {
+        public static List<Person> persons = new List<Person>();
+        public static int idCounter = 0;
+
         [HttpGet]
         [Route("api/Example/Person")]
-        public IEnumerable<Person> Get()
+        public HttpResponseMessage Get()
         {
-            return People.listAll();
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, ExampleController.persons);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.ExpectationFailed, e);
+            }
         }
 
         [HttpGet]
         [Route("api/Example/Person/{id:int}")]
         public HttpResponseMessage GetName(int id)
         {
-            if (People.exists(id))
-                return Request.CreateResponse(HttpStatusCode.OK, People.persons[id]);
+            if (this.exists(id))
+                return Request.CreateResponse(HttpStatusCode.OK, ExampleController.persons[id]);
             else
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Person Not Found");
         }
 
         [HttpPost]
         [Route("api/Example/Person")]
-        public void JsonStringBody([FromBody]Person value)
+        public HttpResponseMessage JsonStringBody([FromBody]Person person)
         {
-            People.addNewPerson(value);
+            try
+            {
+                person.addId(ExampleController.idCounter);
+                ExampleController.idCounter++;
+                ExampleController.persons.Add(person);
+                return Request.CreateResponse(HttpStatusCode.OK, "Person added");
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.ExpectationFailed, e);
+            }
         }
 
         [HttpPut]
         [Route("api/Example/Person/{id:int}")]
         public HttpResponseMessage updateName(int id,[FromBody] string newName) {
-            if (People.exists(id))
+            if (this.exists(id))
             {
-                People.persons[id].changeName(newName);
-                return Request.CreateResponse<Person>(HttpStatusCode.OK, People.persons[id]);
+                ExampleController.persons[id].changeName(newName);
+                return Request.CreateResponse<Person>(HttpStatusCode.OK, ExampleController.persons[id]);
             }
             else
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Person Not Found");
@@ -49,10 +69,10 @@ namespace Example.Webapi.Controllers
         [Route("api/Example/Person/{id:int}")]
         public HttpResponseMessage DeletePerson(int id)
         {
-            if (People.exists(id))
+            if (this.exists(id))
             {
-                Person tmp = People.persons[id];
-                People.persons.RemoveAt(id);
+                Person tmp = ExampleController.persons[id];
+                ExampleController.persons.RemoveAt(id);
                 return Request.CreateResponse<Person>(HttpStatusCode.OK, tmp);
             }
             else
@@ -60,31 +80,12 @@ namespace Example.Webapi.Controllers
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Person Not Found");
             }
         }
-    }
-
-    public static class People
-    {
-        public static List<Person> persons = new List<Person>();
-        public static int idCounter = 0;
-
-        static public void addNewPerson(Person person)
+        public bool exists(int id)
         {
-            person.addId(People.idCounter);
-            People.idCounter++;
-            People.persons.Add(person);
-        }
-        static public void deletePerson(int id)
-        {
-            People.persons.RemoveAt(id);
-        }
-        static public bool exists(int id) { 
-            return (id >= 0 && id < People.persons.Count) ;
-                }
-        static public List<Person> listAll()
-        {
-            return persons;
+            return (id >= 0 && id < ExampleController.persons.Count);
         }
     }
+
 
     public class Person 
     {
